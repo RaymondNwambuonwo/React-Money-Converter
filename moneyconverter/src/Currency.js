37c00cac69e5ad64d5195097288fff6c32cc09a4
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-04-29 14:15:19 -0400

    option menu started

--- moneyconverter/src/Currency.js
+++ moneyconverter/src/Currency.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 
 function Currency() {
-  return <div>Currency Row</div>;
+  return (
+    <div>
+      <input type="number" />
+      <select>
+        <option value="Hi">Hi</option>
+      </select>
+    </div>
+  );
 }
 
 export default Currency;
