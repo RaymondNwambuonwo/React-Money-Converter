b526101178af4fde1ef998952ea058d1f5e468d9
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-04-30 10:42:38 -0400

    api was able to fetch data on rates successfully

--- moneyconverter/src/App.js
+++ moneyconverter/src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { Component, useState, useEffect } from "react";
 import Currency from "./Currency";
 import "./App.css";
 
+const Link = "https://api.exchangeratesapi.io/latest";
+
 function App() {
+  useEffect(() => {
+    fetch(Link)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <>
       <h1>Convert Amount</h1>
