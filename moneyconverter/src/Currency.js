efffed94c8dd92256a0249fd882670360ead3a7f
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-04-30 09:08:36 -0400

    set margins and borders for web page

--- moneyconverter/src/Currency.js
+++ moneyconverter/src/Currency.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 function Currency() {
   return (
     <div>
-      <input type="number" />
+      <input type="number" className="input" />
       <select>
         <option value="Hi">Hi</option>
       </select>
